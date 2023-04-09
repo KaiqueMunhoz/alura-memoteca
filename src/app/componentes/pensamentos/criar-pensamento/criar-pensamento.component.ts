19edb495a1ce46795ba443138b243c9fd1aa8f78
Kaique Munhoz <munhoz13km@gmail.com>
2023-04-09 17:19:44 -0300

    feat: Add Router to CriarPensamentoComponent constructor

--- src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
+++ src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-criar-pensamento',
@@ -6,6 +7,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./criar-pensamento.component.css']
 })
 export class CriarPensamentoComponent {
+
+  constructor(private router: Router) { }
 
   pensamento = {
     id: '1',
